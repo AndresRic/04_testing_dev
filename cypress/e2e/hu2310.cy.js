d15c235a3cdbd1071e19dc09315aa1bc9e405a9d
AndresRic <116377326+AndresRic@users.noreply.github.com>
2023-02-06 20:53:12 -0500

    complete testing of field tipo de identificación on beneficiario acreedor

--- cypress/e2e/hu2310.cy.js
+++ cypress/e2e/hu2310.cy.js
@@ -1,5 +1,5 @@
 const info = {
-	quoteNumber: 341,
+	quoteNumber: 354,
 	vehiculo: {
 		placa: 'QUU-0035',
 		usoVehiculo: 1, //1 = particular, 2= comercial,
@@ -50,7 +50,7 @@ const info = {
 		provincia: 'SALINAS',
 	},
 	id_token:
-		'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJjNmEwYmNhYy1kYTkwLTRkODMtOTE2OC1lOTVmNWVlYjZjODYiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vMDhhODMzMzktOTBlNy00OWJmLTkwNzUtOTU3Y2NkNTYxYmYxL3YyLjAiLCJpYXQiOjE2NzU2ODcyNTAsIm5iZiI6MTY3NTY4NzI1MCwiZXhwIjoxNjc1NjkxMTUwLCJlbWFpbCI6IkFuZHJlcy5NdW5vejAxQE91dHNvdXJjaW5nLmxpYmVydHlzZWd1cm9zLmNvbSIsIm5hbWUiOiJNdcOxb3osIEFuZHLDqXMiLCJub25jZSI6IjY3ODkxMCIsIm9pZCI6ImVjOWNiYWQ0LWNlNmUtNDMxOS05ZDk3LWY0MzNlMzE2NmUzYiIsInByZWZlcnJlZF91c2VybmFtZSI6IkFuZHJlcy5NdW5vejAxQE91dHNvdXJjaW5nLmxpYmVydHlzZWd1cm9zLmNvbSIsInJoIjoiMC5BUU1BT1RPb0NPZVF2MG1RZFpWOHpWWWI4YXk4b01hUTJvTk5rV2pwWDE3cmJJWURBRTQuIiwic3ViIjoiMndOeWNVaHRvZHdHU0hNMHcwNm5SWW9xQVRlNUtULTY5NUU4SDE0aU5JMCIsInRpZCI6IjA4YTgzMzM5LTkwZTctNDliZi05MDc1LTk1N2NjZDU2MWJmMSIsInV0aSI6InR1aEw0NjdEaDBpRWp0UVBGSW5jQUEiLCJ2ZXIiOiIyLjAiLCJlbXBsb3llZWlkIjoibjE2MjI1NDIifQ.ClHQY3_vetuY8hSfSdJOA5xy6ghWCd1zD2B5OiYP3KfomOvH_XZb47MFuZtp7TCvW5GFzom6-JtIrE2J8mR7Cly-uTsgeP3THqeYpa2vct4lUAuewSYrS1VcRv3vB4IBD4c3IKgz_4H26lQj1uCE-PXK3EExtZIs5OM2pSVMHjTlvbCCVBmVr3i1NKwMX4Y9P3_UEk37VfHqMOOeJJVwLkxWMJQgGeJb9v7-Y2hRDpxi2RGyYtdcc2nsIHS_uoMu7niNbpwHUNGWxQGYzpa1107pI65Tenc_RUj4kFPZNZUsx84RKnME6BHa0vcWN6eLGH9htnAUjUA8c6hsCiKAhQ',
+		'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJjNmEwYmNhYy1kYTkwLTRkODMtOTE2OC1lOTVmNWVlYjZjODYiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vMDhhODMzMzktOTBlNy00OWJmLTkwNzUtOTU3Y2NkNTYxYmYxL3YyLjAiLCJpYXQiOjE2NzU3MzI2MjEsIm5iZiI6MTY3NTczMjYyMSwiZXhwIjoxNjc1NzM2NTIxLCJlbWFpbCI6IkFuZHJlcy5NdW5vejAxQE91dHNvdXJjaW5nLmxpYmVydHlzZWd1cm9zLmNvbSIsIm5hbWUiOiJNdcOxb3osIEFuZHLDqXMiLCJub25jZSI6IjY3ODkxMCIsIm9pZCI6ImVjOWNiYWQ0LWNlNmUtNDMxOS05ZDk3LWY0MzNlMzE2NmUzYiIsInByZWZlcnJlZF91c2VybmFtZSI6IkFuZHJlcy5NdW5vejAxQE91dHNvdXJjaW5nLmxpYmVydHlzZWd1cm9zLmNvbSIsInJoIjoiMC5BUU1BT1RPb0NPZVF2MG1RZFpWOHpWWWI4YXk4b01hUTJvTk5rV2pwWDE3cmJJWURBRTQuIiwic3ViIjoiMndOeWNVaHRvZHdHU0hNMHcwNm5SWW9xQVRlNUtULTY5NUU4SDE0aU5JMCIsInRpZCI6IjA4YTgzMzM5LTkwZTctNDliZi05MDc1LTk1N2NjZDU2MWJmMSIsInV0aSI6IjlqX3BIQ0UxekUtWU5YcDlFNDAtQVEiLCJ2ZXIiOiIyLjAiLCJlbXBsb3llZWlkIjoibjE2MjI1NDIifQ.cNyW3PX4_IEXIJKa3h_eFpOeec09ueYUhoLx3_R4Lu1E3rexhlksImm15yKes5bOXvvVU6w3imtBQhRtTPwsu4oAN4ldkkGt3DUE1IxMIXr4uFI8mvK4bz4n-0kOxvqNUTuRCbJ67MwxrlyRVbJH2RghgaAQxw2D9LOYnm4ugdWR7JcRhtLz0g3yt4v9Fb4w5RURhIGpV8VW0bPWUjvcPTblh66sYxEz5H2Q7chb9zeKC2P04m6HmAvioabbztQVCUd00V7xN2V2dgNkOuoQQQFfTlUd9QT9IgJcZqaYGsqdDl3vYyQBbVv8ikuWk3jzZACuY8AZU7GjK7C5KsqNHA',
 };
 
 // describe('Testing HU 2310 - Campos Beneficiario Acreedor - from new quotation', () => {
@@ -240,7 +240,6 @@ describe('Testing HU 2310 - Campos Beneficiario Acreedor - from resume a quotati
 			'https://d05pyp3zy1.execute-api.us-east-1.amazonaws.com/dev/v1/emission/update-emission'
 		).as('update-emission');
 
-		/* ==== Generated with Cypress Studio ==== */
 		cy.get('tbody > tr > :nth-child(2)')
 			.contains(info.quoteNumber)
 			.parent()
@@ -288,21 +287,145 @@ describe('Testing HU 2310 - Campos Beneficiario Acreedor - from resume a quotati
 			.contains('Guardar')
 			.click();
 		cy.wait('@update-emission');
-
-		// cy.get(
-		// 	'.ModalForm__container--content--description > :nth-child(3) > .button--primary'
-		// ).click();
-
-		// cy.get(':nth-child(3) > .mt-auto > .relative > .w-84').click();
-		// cy.get('.ModalForm__container--content--description').click();
-		// cy.get('.break-all').should('be.visible');
-		// cy.get('.break-all').should(
-		// 	'have.text',
-		// 	'Este campo no puede quedar vacío'
-		// );
-
-		/* ==== End Cypress Studio ==== */
+		cy.contains('¿Tiene crédito vehicular?')
+			.parent()
+			.find('input[type="checkbox"]')
+			.check();
+		cy.contains('Rol necesario para emitir la póliza').should('be.visible');
+		cy.contains('Debe seleccionarse un Beneficiario Acreedor').should(
+			'be.visible'
+		);
+		cy.contains('Agregar persona').should('be.visible');
+		cy.get(
+			'img[src="/static/media/plus.c53387e2b062556ee2afe8c284b7ba8e.svg"]'
+		).click();
 	}); ///////////////end of beforeEach
 
-	it('Campo: Tipo de identificación', () => {});
+	it.only('Tests field Tipo de identificación', () => {
+		cy.log('Test of error message'); //test of error message
+		cy.get('button[name = "identificationType"]').as('idTypeField');
+
+		//propiedades del campo en estado predeterminado
+		cy.log('Test of propierties of field on default state');
+		cy.get('@idTypeField')
+			.should('not.have.class', 'btn-dropdown-error')
+			.and('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+			.and('have.css', 'font-family', 'Roboto-Light')
+			.and('have.css', 'font-size', '16px')
+			.and('have.css', 'color', 'rgb(112, 112, 112)')
+			.and('have.css', 'font-weight', '100')
+			.and('have.css', 'border', '1px solid rgb(145, 145, 145)');
+
+		//////////////// Pruebas de campo en error de 'campo vacío'/////////////
+		//click dentro y fuera del campo (sin seleccionar una opción)
+		cy.log('Test of propierties of field on empty state');
+		cy.get('@idTypeField').click();
+		cy.get('ul > button[id=box-identificationNumber]')
+			.as('rucOption')
+			.should('have.text', 'RUC');
+		cy.get('span').contains('Datos Personales').click();
+
+		//propiedades del contenador del campo
+		cy.log('Test of propierties of field container');
+		cy.get('#identificationNumber')
+			.find('img')
+			.should('have.attr', 'alt', 'error')
+			.and('be.visible');
+
+		//propiedades del campo en estado de error 'campo vacío'
+		cy.log('Test of propierties of field on empty error');
+		cy.get('@idTypeField')
+			.should('have.class', 'btn-dropdown-error')
+			.and('have.css', 'background-color', 'rgb(250, 241, 241)')
+			.and('have.css', 'font-family', 'Roboto-Light')
+			.and('have.css', 'font-size', '16px')
+			.and('have.css', 'color', 'rgb(112, 112, 112)')
+			.and('have.css', 'font-weight', '100')
+			.and('have.css', 'border', '1px solid rgb(233, 34, 67)');
+
+		//propiedades del mensaje de error
+		cy.log('Test of propierties of error message of empty error');
+		cy.get('#identificationNumber')
+			.contains('Es necesario realizar una selección')
+			.as('errorMsg');
+		cy.get('@errorMsg')
+			.should('be.visible')
+			.should('have.class', 'text-lmRed')
+			.and('have.css', 'font-family', 'Roboto-Regular')
+			.and('have.css', 'font-size', '12px')
+			.and('have.css', 'color', 'rgb(211, 47, 47)')
+			.and('have.css', 'font-weight', '400');
+
+		//propiedades del campo seleccionando una opción
+		cy.log('Test of propierties of field with option selected');
+		cy.get('@idTypeField').click();
+		cy.get('@rucOption').click();
+		cy.get('@idTypeField')
+			.should('not.have.class', 'btn-dropdown-error')
+			.and('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+			.and('have.css', 'font-family', 'Roboto-Light')
+			.and('have.css', 'font-size', '16px')
+			.and('have.css', 'color', 'rgb(112, 112, 112)')
+			.and('have.css', 'font-weight', '100')
+			.and('have.css', 'border', '1px solid rgb(145, 145, 145)')
+			.and('have.attr', 'title', 'RUC')
+			.and('have.attr', 'value', 'R');
+
+		cy.get('@idTypeField')
+			.find('span')
+			.contains('RUC')
+			.should('have.text', 'RUC')
+			.and('have.css', 'color', 'rgb(52, 55, 65)')
+			.and('have.css', 'font-family', 'Roboto-Regular')
+			.and('have.css', 'font-size', '16px')
+			.and('have.css', 'font-weight', '100');
+	});
+
+	it('Tests field Identificación', () => {
+		cy.get('button[name = "identificationType"]').as('idTypeField');
+		cy.get('@idTypeField').click();
+		cy.get('@idTypeField').blur();
+
+		cy.get('ul > button[id=box-identificationNumber]')
+			.should('have.text', 'RUC')
+			.as('rucOption');
+
+		cy.get('@rucOption').click();
+	});
+
+	it('Tests Button Validar', () => {
+		cy.get('button[name = "identificationType"]').as('idTypeField');
+		cy.get('@idTypeField').click();
+		cy.get('@idTypeField').blur();
+
+		cy.get('ul > button[id=box-identificationNumber]')
+			.should('have.text', 'RUC')
+			.as('rucOption');
+
+		cy.get('@rucOption').click();
+	});
+
+	it('Tests Button Cancelar', () => {
+		cy.get('button[name = "identificationType"]').as('idTypeField');
+		cy.get('@idTypeField').click();
+		cy.get('@idTypeField').blur();
+
+		cy.get('ul > button[id=box-identificationNumber]')
+			.should('have.text', 'RUC')
+			.as('rucOption');
+
+		cy.get('@rucOption').click();
+	});
+
+	it('Tests Button Guardar', () => {
+		cy.get('button[name = "identificationType"]').as('idTypeField');
+		cy.get('@idTypeField').click();
+		cy.get('@idTypeField').blur();
+
+		cy.get('ul > button[id=box-identificationNumber]')
+			.should('have.text', 'RUC')
+			.as('rucOption');
+
+		cy.get('@rucOption').click();
+	});
 });
